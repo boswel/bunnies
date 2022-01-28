d9864d9f784086c0c26d7378b9f21f8f32f886cf
unknown <s.luerzel@gmail.com>
2022-01-28 22:13:01 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+generateRabbit();
+
+function generateRabbit() {
+  let image = document.createElement("img")
+  let container = document.getElementById("bunnyspace");
+  container.appendChild(image);
+
+  randomRabbit = Math.floor(Math.random() * 10);
+  image.src = "./images/bunny" + randomRabbit + ".png";
+  image.alt = "cute rabbit";
+  image.classList.add("rabbit");
+  image.addEventListener("click", generateRabbit);
+
+  let maxBunnyHeight = container.offsetHeight / 100 * 50; //px
+  let lowestPosition = container.offsetHeight - maxBunnyHeight;
+
+  let scalingFactor = Math.random();
+  image.style.height = (maxBunnyHeight * scalingFactor) + "px";
+  image.style.top = (lowestPosition * scalingFactor) + "px";
+
+  let bunnyWidth = parseFloat(getComputedStyle(image).getPropertyValue("height"));
+  let rightmostPosition = container.offsetWidth - bunnyWidth;
+  image.style.left = (rightmostPosition * Math.random()) + "px";
+ 
+  image.style.zIndex = Math.floor(scalingFactor * lowestPosition);
+  image.style.filter = "blur(" + ((1 - scalingFactor) * maxBunnyHeight * 0.004) + "px)";
+}
