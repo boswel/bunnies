e782552118c1e26b0ed199b313666f27068bbbb1
steffi <s.luerzel@gmail.com>
2022-04-16 16:07:46 +0200

    switch between heroku & local db

--- bunniesdb.py
+++ bunniesdb.py
@@ -1,20 +1,29 @@
 import psycopg2
 import psycopg2.extras
 from os import getenv
-# from dotenv import load_dotenv
 
-# load_dotenv()
 
-# dbname = getenv('DBNAME')
-# user = getenv('DBUSER')
-# host = getenv('DBHOST')
-# password = getenv('DBPASSWORD')
+if getenv('DATABASE_URL'):
 
-dbname = getenv('DATABASE_URL')
+    kwargs = {
+        'dbname': getenv('DATABASE_URL'),
+        'sslmode': 'require'
+    }
+
+else:
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    kwargs = {
+        'dbname': getenv('DBNAME'),
+        'user': getenv('DBUSER'),
+        'host': getenv('DBHOST'),
+        'password': getenv('DBPASSWORD')
+    }
 
 
 def connect_to_db():
-    with psycopg2.connect(dbname, sslmode='require') as con:
+    with psycopg2.connect(**kwargs) as con:
         cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
         return (con, cur)
 
